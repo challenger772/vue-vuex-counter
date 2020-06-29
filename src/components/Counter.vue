21ce7c3acc0658e06e4d8976ad6c5278a1bc234a
comet772 <challenger772@outlook.com>
2020-06-29 14:45:27 +0800

    first

---
+++ src/components/Counter.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h2>Welcome to Counter</h2>
+    <div><h3>Counter: </h3>{{ counter }}</div>
+    <button @click="incrementCounter">increment counter</button>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    incrementCounter () {
+      this.$store.dispatch('incrementAction', 1)
+    }
+  },
+
+  computed: {
+    counter () {
+      return this.$store.getters.counter
+    }
+  }
+}
+</script>
